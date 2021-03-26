f419e2ac2085654d9fb31279281da50c67a4cf68
WahAkshat <jeet11chatterjee@gmail.com>
2021-03-26 13:58:05 +0530

    optimized imports

--- src/akshat_tsf/test.java
+++ src/akshat_tsf/test.java
@@ -3,9 +3,7 @@ package akshat_tsf;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class test {
     public static void main(String args[]){
